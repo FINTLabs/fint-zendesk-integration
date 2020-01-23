84cced8b5e87d8dc37a8e7205844c46ca780b631
OlaStalberg <olastaalberg@gmail.com>
2020-01-23 13:56:44 +0100

    added snakecase anotation

--- src/main/java/no/fint/zendesk/model/ticket/Ticket.java
+++ src/main/java/no/fint/zendesk/model/ticket/Ticket.java
@@ -1,5 +1,7 @@
 package no.fint.zendesk.model.ticket;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +13,7 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class Ticket {
     @NotNull
     private String subject;
