92f6eb2c5d981e5ffa4fb49c3008547459347277
Hknots <Hknots99@gmail.com>
2023-08-29 11:46:48 +0200

    add securityConfiguration

---
+++ src/main/java/no/fint/config/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package no.fint.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange((authorize) -> authorize
+                        .pathMatchers("/**")
+                        .authenticated()
+                        .anyExchange()
+                        .authenticated()
+                ).build();
+        return http.build();
+    }
+}
